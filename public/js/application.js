8231b2ac2b4ac6dbbcb58b4d2335d6bbc20f3721
Apprentice <apprentice@dbc-chicago-54.local>
2014-01-29 16:23:07 -0600

    ,

--- public/js/application.js
+++ public/js/application.js
@@ -3,28 +3,30 @@ $(document).ready(function () {
   $('#target').submit(function(event) { // Intecepts form submission
     event.preventDefault(); // Prevents default form-submission
     console.log(this);
-    // var input_from_form = $(this)serialize();
     $.ajax({ // AJAX post
       type: 'Post',
       url: '/rolls',
-      // data: ,
-      // data: input_from_form,
       success: function(data) {
-        $('#die').html(data); // replaces the contents of #die
+        print_num(data) // Replaces the contents of #die
       }
     })
   })
 
-  // function diceRoll(){
-  //   console.log(Roll.new)
-  // }
+  var print_num = function(getRandomNumber) {
+    var randomnumber=Math.floor(Math.random() * 6 + 1);
+    var holder = $('#die');
+    holder.empty()
+    holder.append(randomnumber)
+
+  }
 
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+  //   5- when the AJAX post is done, replace the contents of the
+  //     "#die" DIV in the DOM using jQuery
 
 });
 
